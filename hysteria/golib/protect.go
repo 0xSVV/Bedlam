0a4217706b14425fc0a2ecab8da0b12ea4699f6e
bruh <shapovalov.vw@gmail.com>
2026-04-15 12:09:37 +0300

    Add fd protector

---
+++ hysteria/golib/protect.go
@@ -0,0 +1,42 @@
+package golib
+
+import (
+	"net"
+	"sync"
+)
+
+t type FdProtector interface {
+	Protect(fd int32) bool
+}
+
+var (
+	protectorMu sync.Mutex
+	fdProtector  FdProtector
+)
+
+func SetFdProtector(protector FdProtector) {
+	protectorMu.Lock()
+	defer protectorMu.Unlock()
+	fdProtector = protector
+}
+
+func getFdProtector() FdProtector {
+	protectorMu.Lock()
+	defer protectorMu.Unlock()
+	return fdProtector
+}
+
+func protectPacketConn(conn net.PacketConn) {
+	p := getFdProtector()
+	if p == nil {
+		return
+	}
+	if udpConn, ok := conn.(*net.UDPConn); ok {
+		rawConn, err := udpConn.SyscallConn()
+		if err == nil {
+			rawConn.Control(func(fd uintptr) {
+				p.Protect(int32(fd))
+			})
+		}
+	}
+}
