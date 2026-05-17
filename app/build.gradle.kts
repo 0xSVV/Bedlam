7d64b7ed1bc1fe9b130f01e66adea9b1c8e102e6
shapovalov <shapovalov.vw@gmail.com>
2026-05-17 21:44:36 +0300

    enable kotlinx-serialization plugin for app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.androidx.room)
 }
