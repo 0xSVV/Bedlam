4e5967fd16446403d69c77c2df0fe5db30b05774
bruh <shapovalov.vw@gmail.com>
2026-05-17 20:57:22 +0300

    wire DatabaseModule and ProfileModule into AppComponent

--- app/src/main/kotlin/ru/shapovalov/bedlam/di/AppComponent.kt
+++ app/src/main/kotlin/ru/shapovalov/bedlam/di/AppComponent.kt
@@ -4,19 +4,31 @@ import android.app.Application
 import android.content.Context
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
+import ru.shapovalov.bedlam.core.profile.domain.usecase.DeleteProfileUseCase
+import ru.shapovalov.bedlam.core.profile.domain.usecase.GetProfilesUseCase
+import ru.shapovalov.bedlam.core.profile.domain.usecase.ImportProfileFromUriUseCase
+import ru.shapovalov.bedlam.core.profile.domain.usecase.ObserveActiveProfileUseCase
+import ru.shapovalov.bedlam.core.profile.domain.usecase.ObserveProfileUseCase
+import ru.shapovalov.bedlam.core.profile.domain.usecase.SaveProfileUseCase
+import ru.shapovalov.bedlam.core.profile.domain.usecase.SetActiveProfileUseCase
 import ru.shapovalov.hysteria.HysteriaClientImpl
 import ru.shapovalov.hysteria.api.HysteriaClient
 
-/**
- * Root dependency graph for the process. Built once in `BedlamApplication.onCreate`
- * and held for the lifetime of the application. Feature/screen-level graphs
- * should be modeled as child @Component classes that take this one as a parent.
- */
 @AppScope
 @Component
-abstract class AppComponent(@get:Provides val application: Application) {
+abstract class AppComponent(
+    @get:Provides val application: Application,
+) : DatabaseModule, ProfileModule {
 
     abstract val hysteriaClient: HysteriaClient
+
+    abstract val getProfiles: GetProfilesUseCase
+    abstract val observeProfile: ObserveProfileUseCase
+    abstract val saveProfile: SaveProfileUseCase
+    abstract val deleteProfile: DeleteProfileUseCase
+    abstract val importProfileFromUri: ImportProfileFromUriUseCase
+    abstract val observeActiveProfile: ObserveActiveProfileUseCase
+    abstract val setActiveProfile: SetActiveProfileUseCase
 
     @get:Provides
     val context: Context
