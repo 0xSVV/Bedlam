0d44093e74137f6353c1e0017a56387002434d09
shapovalov <shapovalov.vw@gmail.com>
2026-05-18 21:21:28 +0300

    make Settings a child stack with entry to AppSelection

--- app/src/main/kotlin/ru/shapovalov/bedlam/feature/settings/presentation/SettingsComponent.kt
+++ app/src/main/kotlin/ru/shapovalov/bedlam/feature/settings/presentation/SettingsComponent.kt
@@ -1,10 +1,55 @@
 package ru.shapovalov.bedlam.feature.settings.presentation
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.pushNew
+import com.arkivanov.decompose.value.Value
+import kotlinx.serialization.Serializable
 import me.tatarka.inject.annotations.Assisted
 import me.tatarka.inject.annotations.Inject
+import ru.shapovalov.bedlam.feature.settings.appselection.presentation.AppSelectionComponent
 
 @Inject
 class SettingsComponent(
+    appSelectionFactory: (ComponentContext, AppSelectionComponent.OnBack) -> AppSelectionComponent,
     @Assisted componentContext: ComponentContext,
-) : ComponentContext by componentContext
+) : ComponentContext by componentContext {
+
+    private val navigation = StackNavigation<Config>()
+
+    val childStack: Value<ChildStack<*, Child>> = childStack(
+        source = navigation,
+        serializer = Config.serializer(),
+        initialConfiguration = Config.Root,
+        handleBackButton = true,
+        childFactory = { config, ctx ->
+            when (config) {
+                Config.Root -> Child.Root
+                Config.AppSelection -> Child.AppSelection(
+                    appSelectionFactory(ctx, AppSelectionComponent.OnBack { navigation.pop() })
+                )
+            }
+        },
+    )
+
+    fun onOpenAppSelection() {
+        navigation.pushNew(Config.AppSelection)
+    }
+
+    sealed interface Child {
+        data object Root : Child
+        data class AppSelection(val component: AppSelectionComponent) : Child
+    }
+
+    @Serializable
+    private sealed interface Config {
+        @Serializable
+        data object Root : Config
+
+        @Serializable
+        data object AppSelection : Config
+    }
+}
