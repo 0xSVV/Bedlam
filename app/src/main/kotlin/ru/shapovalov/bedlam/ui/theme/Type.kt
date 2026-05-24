889a6dc6b725dfed637dee891a86448938fe0cc3
shapovalov <shapovalov.vw@gmail.com>
2026-05-24 20:52:46 +0300

    add roboto flex font and edit theme

--- app/src/main/kotlin/ru/shapovalov/bedlam/ui/theme/Type.kt
+++ app/src/main/kotlin/ru/shapovalov/bedlam/ui/theme/Type.kt
@@ -1,5 +1,30 @@
 package ru.shapovalov.bedlam.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
+import androidx.compose.ui.text.font.FontWeight
+import ru.shapovalov.bedlam.R
 
-val BedlamTypography = Typography()
+@OptIn(ExperimentalTextApi::class)
+private fun robotoFlexFont(weight: FontWeight) = Font(
+    resId = R.font.roboto_flex,
+    weight = weight,
+    variationSettings = FontVariation.Settings(FontVariation.weight(weight.weight)),
+)
+
+private val RobotoFlex = FontFamily(
+    robotoFlexFont(FontWeight.Thin),
+    robotoFlexFont(FontWeight.ExtraLight),
+    robotoFlexFont(FontWeight.Light),
+    robotoFlexFont(FontWeight.Normal),
+    robotoFlexFont(FontWeight.Medium),
+    robotoFlexFont(FontWeight.SemiBold),
+    robotoFlexFont(FontWeight.Bold),
+    robotoFlexFont(FontWeight.ExtraBold),
+    robotoFlexFont(FontWeight.Black),
+)
+
+val BedlamTypography = Typography(fontFamily = RobotoFlex)
