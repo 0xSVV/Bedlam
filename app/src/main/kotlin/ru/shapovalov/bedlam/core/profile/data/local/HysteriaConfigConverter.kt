9d11df1b3cc4f0c83f13d4ab4e5aee65bdd4762b
shapovalov <shapovalov.vw@gmail.com>
2026-08-09 00:37:34 +0300

    Decode an unreadable profile row into a placeholder instead of throwing

--- app/src/main/kotlin/ru/shapovalov/bedlam/core/profile/data/local/HysteriaConfigConverter.kt
+++ app/src/main/kotlin/ru/shapovalov/bedlam/core/profile/data/local/HysteriaConfigConverter.kt
@@ -1,9 +1,17 @@
 package ru.shapovalov.bedlam.core.profile.data.local
 
+import android.util.Log
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import kotlinx.serialization.json.Json
 import ru.shapovalov.hysteria.config.HysteriaConfig
+import ru.shapovalov.hysteria.config.ServerCredentials
+import ru.shapovalov.hysteria.config.TlsOptions
+
+val unreadableHysteriaConfig = HysteriaConfig(
+    server = ServerCredentials(),
+    tls = TlsOptions(),
+)
 
 @ProvidedTypeConverter
 class HysteriaConfigConverter(private val json: Json) {
@@ -12,5 +20,14 @@ class HysteriaConfigConverter(private val json: Json) {
     fun toJson(config: HysteriaConfig): String = json.encodeToString(config)
 
     @TypeConverter
-    fun fromJson(value: String): HysteriaConfig = json.decodeFromString(value)
+    fun fromJson(value: String): HysteriaConfig =
+        runCatching { json.decodeFromString<HysteriaConfig>(value) }
+            .getOrElse {
+                Log.w(TAG, "Profile config is unreadable; substituting a blank one", it)
+                unreadableHysteriaConfig
+            }
+
+    private companion object {
+        const val TAG = "HysteriaConfigConverter"
+    }
 }
